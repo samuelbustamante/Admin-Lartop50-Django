af06e59d780e70c948cebc04cbe42099eef7ef52
Samuel Bustamante <samuelbustamant@gmail.com>
2012-10-11 00:30:21 -0300

    update model

--- supercomputers/models.py
+++ supercomputers/models.py
@@ -4,63 +4,155 @@ from django.contrib.auth.models import User
 from django.db import models
 from choices import *
 
+from django.utils.translation import ugettext_lazy as _
+
 class Center(models.Model):
     user = models.ForeignKey(User)
-    name = models.CharField(max_length=50)
-    acronym = models.CharField(max_length=25)
-    segment = models.CharField(max_length=1, choices=SEGMENTS)
-    country = models.CharField(max_length=2, choices=COUNTRIES)
-    city = models.CharField(max_length=50)
-    url = models.URLField(blank=True)
-    description = models.TextField(blank=True)
+    name = models.CharField(max_length=50,
+        verbose_name=_('Name')
+    )
+    acronym = models.CharField(max_length=25,
+        verbose_name=_('Acronym')
+    )
+    segment = models.CharField(max_length=1, choices=SEGMENTS,
+        verbose_name=_('Segment')
+    )
+    country = models.CharField(max_length=2, choices=COUNTRIES,
+        verbose_name=_('Country')
+    )
+    city = models.CharField(max_length=50,
+        verbose_name=_('City')
+    )
+    url = models.URLField(blank=True,
+        verbose_name=_('Website')
+    )
+    description = models.TextField(blank=True,
+        verbose_name=_('Description')
+    )
 
 class System(models.Model):
     center = models.ForeignKey(Center)
-    name = models.CharField(max_length=50)
-    status = models.BooleanField()
-    area = models.CharField(max_length=1, choices=AREAS)
-    description = models.TextField()
-    vendor = models.CharField(max_length=50)
-    year_install = models.CharField(max_length=2, choices=YEARS)
+    name = models.CharField(max_length=50,
+        verbose_name=_('Name')
+    )
+    status = models.BooleanField(
+        verbose_name=_('Status')
+    )
+    area = models.CharField(max_length=1, choices=AREAS,
+        verbose_name=_('Areas')
+    )
+    description = models.TextField(
+        verbose_name=_('Description')
+    )
+    vendor = models.CharField(max_length=50,
+        verbose_name=_('Vendor')
+    )
+    year_install = models.CharField(max_length=2, choices=YEARS,
+        verbose_name=_('Year install')
+    )
 
 class Component(models.Model):
     system = models.ForeignKey(System)
-    name = models.CharField(max_length=50)
-    model = models.CharField(max_length=50)
-    vendor = models.CharField(max_length=50)
-    nodes = models.IntegerField()
-    memory_node = models.IntegerField()
+    name = models.CharField(max_length=50,
+        verbose_name=_('Name')
+    )
+    model = models.CharField(max_length=50,
+        verbose_name=_('Model')
+    )
+    vendor = models.CharField(max_length=50,
+        verbose_name=_('Vendor')
+    )
+    nodes = models.IntegerField(
+        verbose_name=_('Number of nodes')
+    )
+    memory_node = models.IntegerField(
+        verbose_name=_('Memory per node (in Mb)')
+    )
     # PROCESSOR
-    processor_name = models.CharField(max_length=50)
-    processor_model = models.CharField(max_length=50)
-    processor_number = models.IntegerField()
-    processor_num_cores = models.IntegerField()
-    processor_speed = models.FloatField()
+    processor_name = models.CharField(max_length=50,
+        verbose_name=_('Processor name')
+    )
+    processor_model = models.CharField(max_length=50,
+        verbose_name=_('Processor model')
+    )
+    processor_number = models.IntegerField(
+        verbose_name=_('Number of processors')
+    )
+    processor_num_cores = models.IntegerField(
+        verbose_name=_('Number of cores per processor')
+    )
+    processor_speed = models.FloatField(
+        verbose_name=_('Speed processor')
+    )
     # ACCELERATOR
-    accelerator_name = models.CharField(max_length=50 )
-    accelerator_model = models.CharField(max_length=50)
-    accelerator_number = models.IntegerField()
-    accelerator_num_cores = models.IntegerField()
-    accelerator_speed = models.FloatField()
+    accelerator_name = models.CharField(max_length=50,
+        verbose_name=_('Accelerator name')
+    )
+    accelerator_model = models.CharField(max_length=50,
+        verbose_name=_('Accelerator model')
+    )
+    accelerator_number = models.IntegerField(
+        verbose_name=_('Number of accelarators')
+    )
+    accelerator_num_cores = models.IntegerField(
+        verbose_name=_('Number of cores per accelerator')
+    )
+    accelerator_speed = models.FloatField(
+        verbose_name=_('Aceletator speed')
+    )
     # POWER
-    peak_power = models.FloatField()
-    measured_power = models.FloatField()
+    peak_power = models.FloatField(
+        verbose_name=_('Peak power')
+    )
+    measured_power = models.FloatField(
+        verbose_name=_('Measured power')
+    )
     # CONFIGURATION
-    interconection = models.CharField(max_length=50)
-    operating_system = models.CharField(max_length=50)
+    interconection = models.CharField(max_length=50,
+        verbose_name=_('Interconnection')
+    )
+    operating_system = models.CharField(max_length=50,
+        verbose_name=_('Operating system')
+    )
 
 class Linpack(models.Model):
     system = models.ForeignKey(System, unique=True)
-    benchmark_date = models.DateField()
-    number_cores = models.IntegerField()
-    number_gpu_cores = models.IntegerField()
-    rmax = models.FloatField()
-    rpeak = models.FloatField()
-    nmax = models.FloatField()
-    nhalf = models.FloatField()
-    compiler_name = models.CharField(max_length=50)
-    compiler_options = models.CharField(max_length=50)
-    math_library = models.CharField(max_length=50)
-    mpi_library = models.CharField(max_length=50)
-    hpl_input = models.TextField()
-    hpl_output = models.TextField()
+    benchmark_date = models.DateField(
+        verbose_name=_('Benchmark date')
+    )
+    number_cores = models.IntegerField(
+        verbose_name=_('Number of cores')
+    )
+    number_accelerator = models.IntegerField(
+        verbose_name=_('Number of accelerators')
+    )
+    rmax = models.FloatField(
+        verbose_name=_('rmax')
+    )
+    rpeak = models.FloatField(
+        verbose_name=_('rpeak')
+    )
+    nmax = models.FloatField(
+        verbose_name=_('nmax')
+    )
+    nhalf = models.FloatField(
+        verbose_name=_('nhalft')
+    )
+    compiler_name = models.CharField(max_length=50,
+        verbose_name=_('Compiler name')
+    )
+    compiler_options = models.CharField(max_length=50,
+        verbose_name=_('Compier options')
+    )
+    math_library = models.CharField(max_length=50,
+        verbose_name=_('Math library')
+    )
+    mpi_library = models.CharField(max_length=50,
+        verbose_name=_('mpi library')
+    )
+    hpl_input = models.TextField(
+        verbose_name=_('hpl input')
+    )
+    hpl_output = models.TextField(
+        verbose_name=_('hpl output')
+    )
